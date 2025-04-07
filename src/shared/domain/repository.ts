6942fdf214f599c20ca8380bc8e4a5fce78adc02
zufichris <zufichris@gmail.com>
2025-04-08 00:39:06 +0400

    Add IBaseRepository interface for improved data handling and repository pattern implementation

---
+++ src/shared/domain/repository.ts
@@ -0,0 +1,23 @@
+import { ID, QueryParams, QueryResult } from "../application";
+
+export interface IBaseRepository<
+  Entity,
+  Fields extends Array<Extract<keyof Entity, string>>,
+> {
+  create: (
+    data: Omit<Entity, "id" | "createdAt" | "updatedAt">,
+  ) => Promise<Entity>;
+  update: (
+    id: ID,
+    data: Omit<Entity, "id" | "createdAt" | "updatedAt">,
+  ) => Promise<Entity>;
+  count: (filters?: QueryParams<Fields>["filters"]) => Promise<number>;
+  query: (query?: QueryParams<Fields>) => Promise<QueryResult>;
+  findById: (
+    id: ID,
+    fields?: QueryParams<Fields>["fields"],
+    queryOptions?: QueryParams<string[]>["options"],
+  ) => Promise<Entity>;
+  findOne: (query?: QueryParams<Fields>) => Promise<Entity>;
+  delete: (id: string) => Promise<boolean>;
+}
