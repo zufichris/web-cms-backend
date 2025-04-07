bbe440e603d01d79093061361657db0c95c2bbf9
zufichris <zufichris@gmail.com>
2025-04-08 00:40:07 +0400

    Refactor logger middleware for improved readability and consistency

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,25 +1,22 @@
-import { ApiHandler } from "@app/shared";
+import { ApiHandler } from "@app/shared/infrastructure/http/api-handler";
 import { logger } from "@app/utils";
 
 export const loggerMiddleware = ApiHandler(async (req, res, next) => {
-    const start = Date.now();
-    const { method, url, ip } = req;
+  const start = Date.now();
+  const { method, url, ip } = req;
 
-    logger.info(`Incoming ${method.toUpperCase()} ${url} from ${ip}`);
+  logger.info(`Incoming ${method.toUpperCase()} ${url} from ${ip}`);
 
-    if (req.body) {
-        logger.debug('Request body:', req.body);
-    }
+  if (req.body) {
+    logger.debug("Request body:", req.body);
+  }
 
-    res.on('finish', () => {
-        const duration = Date.now() - start;
-        const { statusCode } = res;
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    const { statusCode } = res;
 
-        logger.info(
-            `${method} ${url} returned ${statusCode} in ${duration}ms`
-        );
-    });
+    logger.info(`${method} ${url} returned ${statusCode} in ${duration}ms`);
+  });
 
-    next();
-    Promise.resolve()
+  next();
 });
