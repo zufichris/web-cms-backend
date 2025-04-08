d81a9fa5d29c0e33b89aefb405c19f9f147172d2
zufichris <zufichris@gmail.com>
2025-04-08 21:04:34 +0400

    Add user routes for handling CRUD operations in the user module, integrating UserController and use cases for user management.

---
+++ src/modules/user/infrastructure/http/routes/user.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { UserController } from '@app/modules/user/infrastructure/http/controllers';
+import { QueryUserUseCase, UpdateUserUseCase, GetUserUseCase, DeleteUserUseCase } from '@app/modules/user/domain/use-cases';
+import { CreateUserUseCase } from '@app/modules/user/domain/use-cases';
+import { MongooseUserRepository, UserModel } from '@app/modules/user';
+
+export function createUserRouter(controller: UserController): Router {
+    const router = Router();
+    router.route('/')
+        .get(controller.getAll)
+        .post(controller.create);
+    router.route('/:id')
+        .get(controller.getById)
+        .patch(controller.update)
+        .delete(controller.delete);
+    return router;
+}
+
+
+const userController = new UserController(
+    new CreateUserUseCase(new MongooseUserRepository(UserModel)),
+    new GetUserUseCase(new MongooseUserRepository(UserModel)),
+    new UpdateUserUseCase(new MongooseUserRepository(UserModel)),
+    new DeleteUserUseCase(new MongooseUserRepository(UserModel)),
+    new QueryUserUseCase(new MongooseUserRepository(UserModel)),
+);
+
+export const userRoutes = createUserRouter(userController);
