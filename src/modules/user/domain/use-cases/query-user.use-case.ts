3f44a1b7b50cc7c467bfdc0ff4d8782b5d4dabcf
zufichris <zufichris@gmail.com>
2025-04-09 18:20:07 +0400

    Refactor QueryUserUseCase to extend BaseUseCase, enhancing structure and adding sorting options for user queries.

--- src/modules/user/domain/use-cases/query-user.use-case.ts
+++ src/modules/user/domain/use-cases/query-user.use-case.ts
@@ -1,24 +1,29 @@
 import { User } from '@app/modules/user/domain/entities';
-import { BaseUserUseCase } from '@app/modules/user/domain/use-cases/base';
-import { AuthContext, QueryParams } from '@app/shared';
+import { AuthContext, BaseUseCase, QueryParams } from '@app/shared';
 import { UsecaseResult } from '@app/shared';
 import { logger } from '@app/utils/logger';
+import { IUserRepository } from '../repositories';
 
-export class QueryUserUseCase extends BaseUserUseCase<QueryParams<Array<keyof User>>, User[]> {
-    async execute(input: QueryParams<Array<keyof User>>, context?: AuthContext): Promise<UsecaseResult<User[]>> {
-        try {
-            const result = await this.userRepository.query(input);
-            logger.info(`[${this.constructor.name}] Queried Users`, { count: result.items.length, input, context });
-            return {
-                success: true,
-                message: 'Users queried successfully.',
-                status: 200,
-                data: result.items,
-                meta: { total: result.totalCount, filterCount: result.filterCount, page: input.options?.page ?? 1, limit: input.options?.limit ?? 10 }
-            };
-        } catch (error) {
-            logger.error(`[${this.constructor.name}] Failed to query Users`, { error, input, context });
-            throw error;
-        }
+export class QueryUserUseCase extends BaseUseCase<QueryParams, User[], AuthContext> {
+    constructor(private readonly userRepository: IUserRepository) {
+        super()
+    }
+    async execute(input: QueryParams, context?: AuthContext): Promise<UsecaseResult<User[]>> {
+        const result = await this.userRepository.query(input);
+        logger.info(`[${this.constructor.name}] Queried Users`, { count: result.items.length, input, context });
+        return {
+            success: true,
+            message: 'Users queried successfully.',
+            status: 200,
+            data: result.items,
+            meta: {
+                totalCount: result.totalCount,
+                filterCount: result.filterCount,
+                page: input.options?.page ?? 1,
+                limit: input.options?.limit ?? 10,
+                sort_by: input.options?.sortField ?? "createdAt",
+                sort_dir: (input.options?.sortDir ?? -1) > 0 ? "asc" : "desc"
+            }
+        };
     }
 }
