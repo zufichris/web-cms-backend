a0da1ca0bb2e678c669f84dc6ec33904bfb2d400
zufichris <zufichris@gmail.com>
2025-04-09 18:19:46 +0400

    Refactor CreateUserUseCase to enhance password hashing, validation, and error handling, improving user creation process and security.

--- src/modules/user/domain/use-cases/create-user.use-case.ts
+++ src/modules/user/domain/use-cases/create-user.use-case.ts
@@ -1,26 +1,37 @@
 
 import { CreateUserDto } from '@app/modules/user/application/dtos';
 import { User } from '@app/modules/user/domain/entities';
-import { BaseUserUseCase } from '@app/modules/user/domain/use-cases/base';
-import { UsecaseResult } from '@app/shared';
+import { AppError, BaseUseCase, UsecaseResult } from '@app/shared';
 import { AuthContext } from '@app/shared';
 import { logger } from '@app/utils/logger';
-import { hash } from 'node:crypto';
-export class CreateUserUseCase extends BaseUserUseCase<CreateUserDto, User> {
-    async execute(input: CreateUserDto, context?: AuthContext): Promise<UsecaseResult<User>> {
-        try {
-            logger.todo("Perform Proper Password Hashing");
-            const data = {
-                ...input,
-                password: await hash(input.password, 'sha256'),
-                isActive: true,
-            }
-            const entity = await this.userRepository.create(data);
-            logger.info(`[${this.constructor.name}] Created User`, { id: entity.id, context });
-            return { success: true, message: 'User created successfully.', status: 201, data: entity };
-        } catch (error) {
-            logger.error(`[${this.constructor.name}] Failed to create User`, { error, input, context });
-            throw error;
-        }
+import bcrypt from "bcrypt"
+import { CreateUserValidationSchema } from '../../infrastructure';
+import { UserRole } from '../enums';
+import { IUserRepository } from '../repositories';
+
+export class CreateUserUseCase extends BaseUseCase<CreateUserDto, User, AuthContext> {
+    constructor(private readonly userRepository: IUserRepository) {
+        super()
     }
+    async beforeExecute(input: User): Promise<void> {
+        CreateUserValidationSchema.parse(input)
+    }
+
+    async execute(input: CreateUserDto, context?: AuthContext): Promise<UsecaseResult<User>> {
+        const hashedPassword = await bcrypt.hash(input.password, 10)
+        const data = {
+            ...input,
+            role: UserRole.USER,
+            password: hashedPassword,
+            isActive: true,
+        }
+        const exists = await this.userRepository.findByEmail(input.email).catch(_ => { })
+        if (exists) {
+            throw AppError.conflict("email taken, please try another email")
+        }
+        const entity = await this.userRepository.create(data);
+        logger.info(`[${this.constructor.name}] Created User`, { id: entity.id, context });
+        return { success: true, message: 'User created successfully.', status: 201, data: entity };
+    }
+
 }
