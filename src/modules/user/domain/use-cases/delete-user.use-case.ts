2afec40b46c1302e8aaa2cb44af726f17d457bca
zufichris <zufichris@gmail.com>
2025-04-10 11:47:12 +0400

    Add validation and existence check in DeleteUserUseCase before execution

--- src/modules/user/domain/use-cases/delete-user.use-case.ts
+++ src/modules/user/domain/use-cases/delete-user.use-case.ts
@@ -1,5 +1,5 @@
 
-import { AppError, BaseUseCase } from '@app/shared';
+import { AppError, BaseUseCase, ParamIdValidationSchema } from '@app/shared';
 import { AuthContext } from '@app/shared';
 import { UsecaseResult } from '@app/shared';
 import { logger } from '@app/utils/logger';
@@ -8,6 +8,14 @@ import { IUserRepository } from '../repositories';
 export class DeleteUserUseCase extends BaseUseCase<string, boolean, AuthContext> {
     constructor(private readonly userRepository: IUserRepository) {
         super()
+    }
+    async beforeExecute(id: string, context?: AuthContext): Promise<void> {
+        ParamIdValidationSchema.parse(id)
+        const entity = await this.userRepository.findById(id).catch(_error => { })
+        if (!entity) {
+            logger.warn(`[${this.constructor.name}] User not found`, { id, context });
+            throw AppError.notFound(`User with id '${id}' not found`);
+        }
     }
     async execute(id: string, context?: AuthContext): Promise<UsecaseResult<boolean>> {
         const success = await this.userRepository.delete(id);
