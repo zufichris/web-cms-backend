9e3a0606438e7d2d3dd983d6572045c8a44938ec
zufichris <zufichris@gmail.com>
2025-04-10 12:42:04 +0400

    Fix typo in mongoose import and implement MongoBaseRepository for enhanced database operations

--- src/shared/infrastructure/index.ts
+++ src/shared/infrastructure/index.ts
@@ -1,4 +1,4 @@
 export * from "./http/routes";
 export * from "./http/controller";
 export * from "./http/api-handler";
-export * from "./persistence/mogoose";
+export * from "./persistence/mongoose";
