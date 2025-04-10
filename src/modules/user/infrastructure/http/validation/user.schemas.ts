9e29f4d03f16b2df152f24fd1f94d23970e27a7a
zufichris <zufichris@gmail.com>
2025-04-10 11:47:36 +0400

    Enhance UpdateUserValidationSchemaBody to include ID validation

--- src/modules/user/infrastructure/http/validation/user.schemas.ts
+++ src/modules/user/infrastructure/http/validation/user.schemas.ts
@@ -1,3 +1,4 @@
+import { ParamIdValidationSchema } from '@app/shared';
 import { z } from 'zod';
 const UserCoreSchema = z.object({
     name: z.string().min(1, 'Name cannot be empty').trim(),
@@ -5,9 +6,7 @@ const UserCoreSchema = z.object({
     password: z.string().min(8, 'Password must be at least 8 characters long'),
 });
 
-export const ParamIdValidationSchema = z.object({
-    id: z.string().regex(/^[0-9a-fA-F]{24}$/, 'Invalid ID format'),
-});
-
 export const CreateUserValidationSchema = UserCoreSchema.strict();
-export const UpdateUserValidationSchemaBody = UserCoreSchema.partial().strict();
+export const UpdateUserValidationSchemaBody = UserCoreSchema.partial().extend({
+    id: ParamIdValidationSchema
+}).strict();
