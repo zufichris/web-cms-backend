efc42341e8ec1850f3a795fa35d07765e3b94900
zufichris <zufichris@gmail.com>
2025-04-11 18:24:15 +0400

    Add index file to export all page use cases

---
+++ src/modules/page/domain/use-cases/index.ts
@@ -0,0 +1,6 @@
+
+export * from './create-page.use-case';
+export * from './update-page.use-case';
+export * from './delete-page.use-case';
+export * from './get-page.use-case';
+export * from './query-page.use-case';
