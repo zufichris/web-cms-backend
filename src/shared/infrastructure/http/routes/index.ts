e809b14e2f161ac63cb39401a17aaae021f6d3ab
zufichris <zufichris@gmail.com>
2025-04-11 18:25:45 +0400

    Add page routes to the main router for versioned API

--- src/shared/infrastructure/http/routes/index.ts
+++ src/shared/infrastructure/http/routes/index.ts
@@ -1,10 +1,16 @@
 import { initUserModule } from "@app/modules/user";
 import { routesV1 } from "./v1.routes";
 import { Router } from "express";
+import { initPageModule } from "@app/modules/page";
 
 export function initRoutes(version: string, routes: Router): Router {
     const userRoutes = initUserModule()
+    const pageRoutes = initPageModule()
+
     routes.use(`/${version}/users`, userRoutes)
+    routes.use(`/${version}/pages`, pageRoutes)
+
+
     return routes
 }
 
