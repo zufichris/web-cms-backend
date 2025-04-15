6ffd0b74bdd381a48f46d6e3227d3792d5ce15ff
zufichris <zufichris@gmail.com>
2025-04-15 16:04:40 +0400

    Add modules/fom/application/dtos/index.ts

--- src/modules/fom/application/dtos/fom.dto.ts
+++
@@ -1,6 +0,0 @@
-
-import { z } from 'zod';
-import { CreateFomValidationSchema, UpdateFomValidationSchemaBody} from '@app/modules/fom/infrastructure/http/validation';
-
-export type CreateFomDto = z.infer<typeof CreateFomValidationSchema>;
-export type UpdateFomDto = z.infer<typeof UpdateFomValidationSchemaBody> & { id: string };
