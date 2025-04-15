2fdcc3c2de4da87a217b58e263d2fa51c10568a2
zufichris <zufichris@gmail.com>
2025-04-15 16:04:39 +0400

    Add modules/fom/domain/repositories/index.ts

--- src/modules/fom/domain/repositories/fom.repository.ts
+++
@@ -1,7 +0,0 @@
-
-import { IBaseRepository } from '@app/shared';
-import { Fom } from '@app/modules/fom/domain/entities';
-
-export interface IFomRepository extends IBaseRepository<Fom> {
-    findByName(name: string): Promise<Fom>;
-}
