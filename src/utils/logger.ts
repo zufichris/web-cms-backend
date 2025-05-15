195d6cb691c528123436011b72e878039bae71ac
zufichris <zufichris@gmail.com>
2025-05-15 15:59:20 +0400

    fix/logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,17 @@
+import { env } from "@app/config/env";
+
 export const logger = {
   info(info: string, data?: unknown): void {
-    console.info("\n ℹ️ ", info, "\t", data ?? "", "\n");
+    if (!env.in_prod) console.info("\n ℹ️ ", info, "\t", data ?? "", "\n");
   },
   todo(info: string): void {
-    console.warn("\n 📝 TODO:", info, "\n");
+    if (!env.in_prod) console.warn("\n 📝 TODO:", info, "\n");
   },
   warn(info: string, data?: unknown): void {
-    console.warn("\n ⚠️ Warning:", info, "\n", data);
+    if (!env.in_prod) console.warn("\n ⚠️ Warning:", info, "\n", data);
   },
   debug(info: string, data?: unknown): void {
-    console.debug("\n 🔍 ", info, "\t", data ?? "", "\n");
+    if (!env.in_prod) console.debug("\n 🔍 ", info, "\t", data ?? "", "\n");
   },
   error(msg: string, error?: unknown) {
     console.error("\n ❌ ", msg, "\t", error ?? "", "\n");
